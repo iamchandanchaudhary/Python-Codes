234cc5368208d054001bdae605e6354f6d1af779
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2026-02-14 10:37:10 +0530

    String Methods

---
+++ Practice 2.0/String-Slicing.py
@@ -0,0 +1,8 @@
+text = "Python Programming"
+
+print(text[0:6])     # Python         (start to index 5)
+print(text[7:])      # Programming    (index 7 to end)
+print(text[:6])      # Python         (start to index 5)
+print(text[::2])     # Pto rgamn      (every 2nd character)
+print(text[::-1])    # gnimmargorP nohtyP  (reversed)
+print(text[0:10:2])  # Pto r          (start to 9, step 2)P
